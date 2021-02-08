24075ab8af12d8a44072a82f379c2650e95bc8ec
Takahiro <tkhr_621@docomo.ne.jp>
2021-02-08 22:56:39 +0900

    fix

--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -32,7 +32,7 @@ export const Row = ({ title, fetchUrl, isLargeRow }: Props) => {
     fetchData()
   }, [fetchUrl])
 
-  console.log(movies)
+  // console.log(movies)
 
   return (
     <div className="Row">
