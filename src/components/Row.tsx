9c3133119698300403c1696ccf72b6654baf3a1a
Takahiro <tkhr_621@docomo.ne.jp>
2021-02-13 23:02:02 +0900

    feat: youtube表示

--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react' // eslint-disable-line
+import YouTube from 'react-youtube'
 import axios from '../axios'
 import './Row.scss'
 
 const baseUrl = 'https://image.tmdb.org/t/p/original'
+const API_KEY = process.env.REACT_APP_API_KEY
 
 type Props = {
   title: string
@@ -19,8 +21,17 @@ type Movie = {
   backdrop_path: string
 }
 
+type Options = {
+  height: string
+  width: string
+  playerVars: {
+    autoplay: 0 | 1 | undefined
+  }
+}
+
 export const Row = ({ title, fetchUrl, isLargeRow }: Props): JSX.Element => {
   const [movies, setMovies] = useState<Movie[]>([])
+  const [trailerUrl, setTrailerUrl] = useState<string | null>('')
 
   useEffect(() => {
     const fetchData = async () => {
@@ -32,12 +43,40 @@ export const Row = ({ title, fetchUrl, isLargeRow }: Props): JSX.Element => {
     fetchData()
   }, [fetchUrl])
 
+  const opts: Options = {
+    height: '390',
+    width: '640',
+    playerVars: {
+      autoplay: 1,
+    },
+  }
+
+  const handleClick = async (movie: Movie) => {
+    console.log(movie)
+    if (trailerUrl) {
+      setTrailerUrl('')
+    } else {
+      const trailerurl = await axios.get(
+        `/movie/${movie.id}/videos?api_key=${API_KEY}`,
+      )
+      setTrailerUrl(trailerurl.data.results[0]?.key)
+      console.log(trailerurl.data.results[0]?.key)
+    }
+  }
+
+  // const handleKeyDown = (e: any, movie: Movie) => {
+  //   if (e.keycode === 13) {
+  //     handleClick(movie)
+  //   }
+  // }
+
   return (
     <div className="Row">
       <h2 className="Row-title">{title}</h2>
       <div className="Row-posters">
         {/* ポスターコンテンツ */}
         {movies.map((movie, i) => (
+          /* eslint-disable */
           <img
             key={movie.id}
             className={`Row-poster ${i} ${isLargeRow && 'Row-poster-large'}`}
@@ -45,9 +84,11 @@ export const Row = ({ title, fetchUrl, isLargeRow }: Props): JSX.Element => {
               isLargeRow ? movie.poster_path : movie.backdrop_path
             }`}
             alt={movie.name}
+            onClick={() => handleClick(movie)}
           />
         ))}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   )
 }
